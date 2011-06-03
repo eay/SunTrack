c26c1d9ae31c316fa7b6cc54d10107371fafbcaa
Eric Young <eay@pobox.com>
2011-06-03 14:45:42 +1000

    Add basic vector class

--- sol.rb
+++ sol.rb
@@ -1,28 +1,94 @@
 #!/usr/bin/env ruby
 #
 
-include Math
+class Vec3
+  include Math
 
-# Given an angle, what is the reduction in intensity of the sun due to
-# less surface area.
-def angle_effect(angle)
-  t = tan(PI*angle/180)
-  e = t / sqrt(t*t + 1)
+  attr_reader :x,:y,:z
+
+  def initialize(x, y, z)
+    @x = x.to_f
+    @y = y.to_f
+    @z = z.to_f
+  end
+
+  def length
+    sqrt(x*x+y*y+z*z)
+  end
+
+  def normalize!
+    l = length
+    @x, @y, @z = @x/l, @y/l, @z/l
+    self
+  end
+
+  def normalize
+    dup.normalize!
+  end
+
+  # Dot product, it returns the cos of the angle between the vectors
+  # It is the length of the projection of one vector onto the other.
+  def dot(a)
+    @x*a.x + @y*a.y + @z*a.z
+  end
+
+  # Angle between the vectors
+  def angle(a)
+    acos(dot(a))
+  end
+
+  # Given 2 vectors, what is the overlape relative to the 2 vectors
+  def effect(a)
+    normalize.dot(a.normalize)
+  end
+
+  # If the vectors are the ground and the sun, what is the 'power' relative to 1.
+  def shine(a)
+    1.0 - effect(a)
+  end
+
+  def cross(a)
+  puts to_s
+  puts a.to_s
+    self.class.new(
+        @y*a.z - @z*a.y,
+        @z*a.x - @x*a.z,
+        @x*a.y - @y*a.x)
+  end
+
+  # Normal vector to a place (defined by 2 vector
+  def self.normal(a,b)
+    a.cross(b)
+  end
+
+  def to_s
+    "[%1.4f %1.4f %1.4f]" % [@x,@y,@z]
+  end
+end
+
+u = Vec3.new(1,1,0)
+v = Vec3.new(0,1,0)
+
+puts u.cross(v)
+exit
+
+
+u = Vec3.new(1,0,0)
+v = Vec3.new(1,Math::sqrt(3),0).normalize
+
+puts "Affect = #{u.effect(v)}"
+puts u.angle(v)/Math::PI*180
+puts v.angle(u)/Math::PI*180
+
+v = Vec3.new(Math::sqrt(3),1,0).normalize
+puts "Affect = #{u.effect(v)}"
+puts u.angle(v)/Math::PI*180
+puts v.angle(u)/Math::PI*180
+
+puts Math::cos(Math::PI/4 - v.angle(u))
+
+(0..100).each do |y|
+  puts "shine #{y} => #{u.shine(Vec3.new(1,y,0))}"
 end
 
 
-5.step(90,5) do |angle|
-  printf("angle = %2d, efficency = %0.2f\n",angle,angle_effect(angle));
-end
-
-puts "Brisbane, noon, summer => #{angle_effect(85)}"
-puts "Brisbane, noon, winter => #{angle_effect(39)}"
-
-[16,34,40,36].each do |angle|
-  printf("angle = %2d, efficency = %0.2f\n",angle,angle_effect(angle));
-end
-puts
-
-[38,64,83,73.5].each do |angle|
-  printf("angle = %2d, efficency = %0.2f\n",angle,angle_effect(angle));
-end
